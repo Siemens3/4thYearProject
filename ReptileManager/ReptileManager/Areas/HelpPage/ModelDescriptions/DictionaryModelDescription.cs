53675759cfe73d10053102a41d3e286dc2aa9345
Siemens3 <steo.kenny@hotmail.co.uk>
2014-11-05 00:37:27 +0000

    Deleting old files
    Removing old files

--- ReptileManager/ReptileManager/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ReptileManager.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- ReptileManager/ReptileManager/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ReptileManager.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
