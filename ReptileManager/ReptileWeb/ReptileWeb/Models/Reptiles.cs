8dddad893ba4481dd11d852df26c92d5c7f6cc98
Siemens3 <steo.kenny@hotmail.co.uk>
2014-11-07 11:23:20 +0000

    ReptileWebsite
    ReptileWebsite MVC  CRUD implemented.

---
+++ ReptileManager/ReptileWeb/ReptileWeb/Models/Reptiles.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReptileWeb.Models
+{
+    public enum Gender {Male, Female, Unknow}
+    public class Reptile
+    {
+        public String Id { get; set; }
+        public Gender Gender { get; set; }
+        public String SpeciesName { get; set; }
+        public String ScientificName { get; set; }
+        public String CommonName { get; set; }
+        public String Born { get; set; }
+        public String Morph { get; set; }
+        public Boolean Venomous { get; set; }
+
+    }
+
+}
