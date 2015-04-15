aeb8e17297b170d2fb4eadce591394711593f1fc
Siemens3 <steo.kenny@hotmail.co.uk>
2015-04-16 00:12:20 +0100

    Added unit test classes
    Added unit test classes empty at the moment.

---
+++ ReptileManager/ReptileManagerTests/Services/HealthStatusTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReptileManager.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReptileManager.Models;
+namespace ReptileManager.Services.Tests
+{
+    [TestClass()]
+    public class HealthStatusTests
+    {
+        
+         [TestMethod()]
+        public void GetHealthTest()
+        {
+
+            Assert.Fail();
+        }
+
+       
+    }
+}
